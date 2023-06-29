f99e483d19cd531b57273af1416396da9e22451c
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-06-29 08:56:44 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,6 +1,18 @@
 import logging
-from Config import *
-from pyromod import listen
+import os 
+from dotenv import load_dotenv 
+  
+  
+if os.path.exists(".env"): 
+    load_dotenv(".env") 
+
+ENV = bool(os.environ.get("ENV", False)) 
+
+if ENV or os.path.exists(".env"): 
+    from sample_config import *  # noqa 
+elif os.path.exists("config.py"): 
+    from config import *
+
 from pyrogram import Client, idle
 from pyrogram.errors import (
     ApiIdInvalid, AccessTokenInvalid, ApiIdPublishedFlood)
